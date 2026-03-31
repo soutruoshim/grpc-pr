38a23539d299f0535275185e387abd08230a56fb
srhdp <srhdp@BIT-0157.local>
2026-03-31 10:42:44 +0700

    first commit

---
+++ proto/src/test/java/com/srhdp/proto/ProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srhdp.proto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/srhdp/grpc_trading/GrpcTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srhdp.grpc_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
