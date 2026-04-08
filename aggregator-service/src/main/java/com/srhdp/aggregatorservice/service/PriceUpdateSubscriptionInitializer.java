ef1f41c222014479d78b2ddac5b20fc7eb6899a9
srhdp <srhdp@BIT-0157.local>
2026-04-08 09:34:18 +0700

    stream stock

---
+++ aggregator-service/src/main/java/com/srhdp/aggregatorservice/service/PriceUpdateSubscriptionInitializer.java
@@ -0,0 +1,25 @@
+package com.srhdp.aggregatorservice.service;
+
+import com.google.protobuf.Empty;
+import com.srhdp.stock.StockServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PriceUpdateSubscriptionInitializer implements CommandLineRunner {
+
+    @GrpcClient("stock-service")
+    private StockServiceGrpc.StockServiceStub stockClient;
+
+    @Autowired
+    private PriceUpdateListener listener;
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.stockClient
+                .withWaitForReady()
+                .getPriceUpdates(Empty.getDefaultInstance(), listener);
+    }
+}
